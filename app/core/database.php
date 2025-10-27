0644f6e3bcc6a031d48dfb2769b1f3e9ae49ac11
NantenainaRld <nantenainarld@gmail.com>
2025-10-27 17:32:26 +0300

    Finish database.php

---
+++ app/core/database.php
@@ -0,0 +1,52 @@
+<?php
+
+// config file
+require_once __DIR__ . '/../config/config.php';
+
+// CLASS database : open connection - execute query
+class Database
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        // open connection
+        $this->pdo = getConnection();
+    }
+
+    // execute SELECT query and return result
+    public function query($sql, $params = [])
+    {
+        try {
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute();
+
+            return $stm->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            return [];
+        }
+    }
+    // execute INSERT, DELETE, UPDATE query
+    public function execute($sql, $params = [])
+    {
+        try {
+            $stm = $this->pdo->prepare($sql);
+            return $stm->execute($params);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    // handle ERRORs in DEV mode
+    public function handleError(PDOException $e)
+    {
+        // error for DEV mod
+        if (defined('DEBUG') && DEBUG === true) {
+            die('SQL error : ' . $e->getMessage());
+        }
+        // error for PROD mod
+        else {
+            die('Une erreur est survenue.');
+        }
+    }
+}
