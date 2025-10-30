a08d0813ba53327a19f096787fcd71312622486c
NantenainaRld <nantenainarld@gmail.com>
2025-10-30 12:11:27 +0300

    update btn login error

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //btn-login
   const btnLogin = document.getElementById("btn-login");
-  btnLogin.addEventListener("click", () => {
-    alert();
+  btnLogin.addEventListener("click", async () => {
+    //   try {
+    //     //==call api add user
+    //     const responseAddUser = await apiRequest("/user/add_user", {
+    //       method: "POST",
+    //       body: {
+    //         nom_utilisateur: "Anarana",
+    //         prenoms_utilisateur: "fanampipny",
+    //         sexe_utilisateur: "féminin",
+    //         role: "admin",
+    //         email_utilisateur: "esa@a.com",
+    //         mdp: "mdpsss",
+    //         mdpConfirm: "mdpsss",
+    //       },
+    //     });
+    //     console.log(responseAddUser);
+    //   } catch (Error) {
+    //     console.log("Error : " + Error);
+    //   }
   });
 });
