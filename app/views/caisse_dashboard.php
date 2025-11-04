6118c6270b10aacc0808a7c6257e45ca2cbf6611
NantenainaRld <nantenainarld@gmail.com>
2025-11-04 11:54:01 +0300

    add caisse-dashboard.js

---
+++ app/views/caisse_dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <!-- JS base URL -->
+    <script>
+        const SITE_URL = "<?= SITE_URL ?>";
+    </script>
+    <!-- helper js  -->
+    <script src="<?= SITE_URL ?>/js/helper.js"></script>
+</head>
+
+<body>
+    <?= "caisse dashboard"; ?>
+    <button id='test'>Test</button>
+    <!-- script js -->
+    <script src="<?= SITE_URL ?>/js/caisse-dashboard.js"></script>
+</body>
+
+</html>
